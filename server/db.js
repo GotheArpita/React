720b7cd91d987cd01d6598cae15e3e28e885e91e
Arpita <gothearpita232gmail.com>
2026-06-17 16:46:46 +0530

    role added to db

--- server/db.js
+++ server/db.js
@@ -8,15 +8,34 @@ const db = new sqlite3.Database("./users.db", (err) => {
 
         db.run(`
             CREATE TABLE IF NOT EXISTS users (
-                username TEXT PRIMARY KEY,
-                password TEXT NOT NULL,
-                name TEXT NOT NULL
+               username TEXT PRIMARY KEY,
+               password TEXT NOT NULL,
+               name TEXT NOT NULL,
+               role TEXT NOT NULL DEFAULT 'user'
             )
         `, (err) => {
             if (err) {
                 console.error("Error creating table:", err.message);
             } else {
                 console.log("Users table ready");
+
+                db.run(
+                    `INSERT OR IGNORE INTO users (username, password, name, role)
+                     VALUES (?, ?, ?, ?)`,
+                    [
+                        "admin@gmail.com",
+                        "admin123",
+                        "Admin",
+                        "admin"
+                    ],
+                    (err) => {
+                        if (err) {
+                            console.error("Error creating admin:", err.message);
+                        } else {
+                            console.log("Default admin ready");
+                        }
+                    }
+                );
             }
         });
     }
